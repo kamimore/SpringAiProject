761ab49281f4ef7cdeea4f91407cd5e5a4e6df2f
kamimore <negivipin996@gmail.com>
2025-02-25 12:29:54 +0530

    generating stock images

--- src/main/java/com/ai/SpringAiDemo/GenAIController.java
+++ src/main/java/com/ai/SpringAiDemo/GenAIController.java
@@ -1,21 +1,45 @@
 package com.ai.SpringAiDemo;
 
+import java.io.IOException;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.ai.image.ImageResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 public class GenAIController {
 
     private final ChatService chatService;
+    private final ImageService imageService;
 
-    public GenAIController(ChatService chatService) {
+    public GenAIController(ChatService chatService, ImageService imageService) {
         this.chatService = chatService;
+        this.imageService = imageService;
     }
 
     @GetMapping("ask-ai")
     public String getResponse(@RequestParam String prompt) {
         return chatService.getResponse(prompt);
-    } 
+    }
+
+    @GetMapping("generate-image")
+    public void generateImages(HttpServletResponse response, @RequestParam String prompt) throws IOException {
+        ImageResponse imageResponse = imageService.generateImage(prompt);
+        String imageUrl = imageResponse.getResult().getOutput().getUrl();
+        response.sendRedirect(imageUrl);
+    }
+
+    @GetMapping("generate-image-option")
+    public List<String> generateImagesOptions(HttpServletResponse response, @RequestParam String prompt) {
+        ImageResponse imageResponse = imageService.generateImage(prompt);
+
+        return imageResponse.getResults().stream().map(result -> result.getOutput().getUrl()).toList();
+    }
 
 }
